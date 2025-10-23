b357cb78aa0d82ff0a2a77e3f47df08ff4b54297
rafinugraha24 <rafinugraha.lp3i.krw24@gmail.com>
2025-10-23 19:40:46 +0700

    mencoba

---
+++ lib/toko/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class test extends StatelessWidget {
+  const test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mencoba Flutter")),
+    );
+  }
+}
