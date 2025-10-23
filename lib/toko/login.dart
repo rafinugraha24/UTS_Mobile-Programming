1be338b4962752c0a4b33fb6a05f2f7b995d8b04
rafinugraha24 <rafinugraha.lp3i.krw24@gmail.com>
2025-10-23 21:21:34 +0700

    nambah nama controller

--- lib/toko/login.dart
+++ lib/toko/login.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Coba extends StatelessWidget {
-  const Coba ({super.key});
+class TextBoxPage extends StatefulWidget {
+  const TextBoxPage({super.key});
+
+  @override
+  State<TextBoxPage> createState() => _TextBoxPageState();
+}
+
+class _TextBoxPageState extends State<TextBoxPage> {
+  final TextEditingController _namacontroller = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +20,29 @@ class Coba extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Row(
               children: [
-                  Center(
-                    child: Container(
-                      height: 120.0,
-                      width: 120.0,
-                      alignment: Alignment(0, 0),
-                      child: Image(image: AssetImage('gambar/Logo.png'))),
-                  ),
+                  Container(
+                    height: 120.0,
+                    width: 120.0,
+                    alignment: Alignment(0, 0),
+                    child: Image(image: AssetImage('gambar/Logo.png'))),
+
+                  Text("Nama :"),
+                    TextField(
+                      controller: _namacontroller,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        // labelText: "Sandi",
+                        hintText: "Masukan Nama Lengkap",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )
+                      ),
+                    ),
+
+
                 ],
             ),
         ),
       );
+    }
   }
-}
